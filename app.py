ad834be7bbc97c9982102caed8142bb33f3b34a6
Karan Kosta <153578920+NeptuneKaran@users.noreply.github.com>
2025-01-20 22:13:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title("Fine-Tuning BERT for Twitter Sentiment Classification")
+
+classifier = pipeline('text-classification', model='AcheronKaran/distilbert-base-uncased-sentiment-model')
+
+text = st.text_area("Enter Your Tweet Here")
+text = text[:512]
+
+if st.button("Predict"):
+        result = classifier(text)
+        st.write("Prediction Result:", result)
+
